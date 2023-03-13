0180a0f7577049987fe87bf82ad39e12970fe235
John-James Mitchell <johnj.mitchell@icloud.com>
2023-03-13 21:23:26 +0000

    dropdown select box with event placeholders. TODO-read eventId from firestore

--- src/screens/DevScreen/DevScreen.js
+++ src/screens/DevScreen/DevScreen.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
 import axios from 'axios';
 
 export default function DevScreen(props) {
   const [id, setId] = useState('');
+  const [selected, setSelected] = useState('');
   const [data, setData] = useState(null);
+
+  const items = [
+    {key:'1', value:'Parklife'},
+    {key:'2', value:'Leeds Fest'},
+    {key:'3', value:'Boomtown'},
+    {key:'4', value:'Bloodstock 2023'}
+  ] 
 
   const handleClick = async () => {
     try {
@@ -19,11 +28,17 @@ export default function DevScreen(props) {
 
   return (
     <View>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={items}
+        save='value'
+      />
+      
       <Button title="Buy Ticket" onPress={handleClick} />
       {data && data.ticketExists ? (
         <QRCode value={JSON.stringify(data.qrPayload)} size={200} />
       ) : (
-        <Text>Ticket does not exist</Text>
+        <Text>Ticket Purchased</Text>
       )}
     </View>
   );
