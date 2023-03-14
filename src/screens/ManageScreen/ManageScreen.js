870fa64c5f27cd232210152306cee27ebdf3fa2b
RokaJosh <j2muchcontact@gmail.com>
2023-03-14 19:18:51 +0000

    YEEEEEEEEEEEEEEEEEE

--- src/screens/ManageScreen/ManageScreen.js
+++ src/screens/ManageScreen/ManageScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { SafeAreaView, StatusBar, StyleSheet, FlatList, SectionList, Keyboard, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
+import { SafeAreaView, StatusBar, StyleSheet, FlatList, SectionList, Keyboard, Text, TextInput, TouchableOpacity, View, Button, ActivityIndicator } from 'react-native'
 import NavButton from '../../Components/NavButton'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
@@ -10,162 +10,166 @@ export default function ManageScreen(props) {
   const user = props.extraData
   const user_tickets = user.ticketsOwned
 
-  var activeTickets = []
+  var activeTickets = undefined
   var usedTickets = []
 
   const activeTicketsCombined = {title: "Active", data: []}
   const usedTicketsCombined = {title: "Used", data: []}
   var allTickets = []
 
-  const completedTaskData = [{
-    title: "Completed Tasks",
-    data: [
-      {
-        id: "6",
-        task: "Make a section list tutorial"
-      },
-      {
-        id: "7",
-        task: "Share this tutorial"
-      },
-      {
-        id: "8",
-        task: "Ask doubt in the Comments"
-      },
-      {
-        id: "9",
-        task: "Subscribe to logrocket"
-      },
-      {
-        id: "10",
-        task: "Read next Article"
-      },
-      {
-        id: "11",
-        task: "Read next Article 2"
-      },
-      {
-        id: "12",
-        task: "Read next Article 3"
-      },
-      {
-        id: "13",
-        task: "Read next Article 4"
-      },
-      {
-        id: "14",
-        task: "Read next Article 5"
-      },
-      {
-        id: "15",
-        task: "Read next Article 6"
-      },
-      {
-        id: "16",
-        task: "Read next Article 7"
-      },
-      {
-        id: "17",
-        task: "Read next Article 8"
-      },
-      {
-        id: "18",
-        task: "Read next Article 9"
-      },
-      {
-        id: "19",
-        task: "Read next Article 10"
-      },
-    ]
-  }];
+  const { data } = useData(user_tickets)
 
+  /*
   FetchTickets(user_tickets)
   .then(data => {
     data.forEach(ticket => {
       const newTicket = {
         encryptedData: ticket.qrPayload.encryptedData,
-        cost: ticket.ticketMeta.Cost,
-        event: ticket.ticketMeta.Event,
-        seat: ticket.ticketMeta.SeatNumber
+        cost: ticket.ticketMeta.cost.integerValue,
+        event: ticket.ticketMeta.event.stringValue,
       }
-      if(!ticket.ticketMeta.Used.booleanValue)
+      if(!ticket.ticketMeta.used.booleanValue)
       {
-        activeTicketsCombined.data.push({task: ticket.ticketMeta.Event.stringValue})
+        if(!activeTickets)
+        {
+          activeTickets = []
+        }
+        activeTickets.push(ticket.ticketMeta.event.stringValue)
+        activeTicketsCombined.data.push({task: ticket.ticketMeta.event.stringValue})
       }
       else
       {
-        usedTicketsCombined.data.push({task: ticket.ticketMeta.Event.stringValue})
+        usedTickets.push(ticket.ticketMeta.event.stringValue)
+        usedTicketsCombined.data.push({task: ticket.ticketMeta.event.stringValue})
       }
     });
   })
   .then(data => {
-    console.log(activeTicketsCombined)
+    //console.log("Finished:")
+    //console.log(activeTicketsCombined)
   })
   .catch(error => error)
+  */
   
 
   useEffect(() => {
     props.navigation.setOptions({ headerTitle: "Manage Tickets", });
   }, []);
 
+  //if(!data) return <ActivityIndicator size="large" />;
+
+  //const ticketNames = []
+  //data.forEach(ticket => {
+  //  ticketNames.push(ticket.ticketMeta.event.stringValue)
+  //});
+
   const Tab = createMaterialTopTabNavigator();
 
-  console.log(activeTicketsCombined)
-
   return(
-      /*<View style={styles.container}>
-        <Tab.Navigator onPress={() => console.log("fortnite")}>
-          <Tab.Screen
-            name="Active"
-            listeners={{ tabPress: e => {
-              ticketNames.pop()
-            }}}
-            children={props => <ActiveTicketsTab
-            ticketNames={ticketNames} {...props} />}
-          />
-          <Tab.Screen name="Used" component={UsedTicketsTab} />
-        </Tab.Navigator>
-      </View>*/
     <View style={styles.container}>
-      <SectionList
-        sections={[activeTicketsCombined, ...completedTaskData]}
-        extraData={refresh}
-        renderItem={({item})=>(
-            TicketEntry(item.task)
-        )}
-        renderSectionHeader={({section})=>(
-          <Text style={styles.taskTitle}>{section.title}</Text>
-        )}
-        keyExtractor={item=>item.id}
-        stickySectionHeadersEnabled
-      />
+      <Tab.Navigator>
+        <Tab.Screen name="Active" children={()=>
+        {
+          if(!data) return <ActivityIndicator size="large" color="#123123"/>;
+
+          /*
+          const ticketNames = []
+          data.forEach(ticket => {
+            console.log(ticket)
+            ticketNames.push(ticket.ticketMeta.event.stringValue)
+          });
+          */
+
+          const activeTickets = GetActiveTickets(data)
+        
+          return(
+            <View style={styles.container}>
+              <FlatList data={activeTickets.eventNames} renderItem={({item}) => (<Item title={item}/>)}/>
+            </View>
+          )
+        }}/>
+        <Tab.Screen name="Used" children={()=>
+        {
+          if(!data) return <ActivityIndicator size="large" color="#123123"/>;
+
+          const usedTickets = GetUsedTickets(data)
+        
+          return(
+            <View style={styles.container}>
+              <FlatList data={usedTickets.eventNames} renderItem={({item}) => (<Item title={item}/>)}/>
+            </View>
+          )
+        }}/>
+      </Tab.Navigator>
     </View>
   )
 }
 
-function ActiveTicketsTab(props) {
-  return(
-    <View style={styles.container}>
-      <FlatList data={props.ticketNames} renderItem={({item}) => (<Item title={item}/>)}/>
-      <Button onPress={() => console.log(props.ticketNames)} title="get ticket names"/>
-    </View>
-  )
+function GetActiveTickets(tickets)
+{
+  const activeTickets = {tickets: [], eventNames: []}
+  tickets.forEach(ticket => {
+    if(!ticket.ticketMeta.used.booleanValue)
+      {
+        const newTicket = {
+          encryptedData: ticket.qrPayload.encryptedData,
+          cost: ticket.ticketMeta.cost.integerValue,
+          event: ticket.ticketMeta.event.stringValue,
+        }
+        activeTickets.tickets.push(newTicket)
+        activeTickets.eventNames.push(ticket.ticketMeta.event.stringValue)
+      }
+  });
+  return activeTickets
 }
 
-function UsedTicketsTab() {
-
-  return(
-    <Text>Used Tickets: </Text>
-  )
+function GetUsedTickets(tickets)
+{
+  const usedTickets = {tickets: [], eventNames: []}
+  tickets.forEach(ticket => {
+    if(ticket.ticketMeta.used.booleanValue)
+      {
+        const newTicket = {
+          encryptedData: ticket.qrPayload.encryptedData,
+          cost: ticket.ticketMeta.cost.integerValue,
+          event: ticket.ticketMeta.event.stringValue,
+        }
+        usedTickets.tickets.push(newTicket)
+        usedTickets.eventNames.push(ticket.ticketMeta.event.stringValue)
+      }
+  });
+  return usedTickets
 }
+
+export const useData = (user_tickets) => {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    const dataFetch = async () => {
+        var urls = []
+        user_tickets.forEach(ticket => {
+          urls.push('https://us-central1-qrapp-fe2f3.cloudfunctions.net/getTicket?id=' + ticket)
+        });
+        const responsesJSON = await Promise.all(urls.map(url =>
+          fetch(url)
+        ))
+        const data = await Promise.all(responsesJSON.map(r => r.json()));
+
+        setState(data)
+    };
+
+    dataFetch();
+  }, [user_tickets]);
+
+  return { data: state };
+};
 
 const FetchTickets = async (user_tickets) => {
   try {
     var urls = []
     user_tickets.forEach(ticket => {
-      urls.push('https://us-central1-qrapp-fe2f3.cloudfunctions.net/encryptTicket?id=' + ticket)
+      urls.push('https://us-central1-qrapp-fe2f3.cloudfunctions.net/getTicket?id=' + ticket)
     });
-
     const responsesJSON = await Promise.all(urls.map(url =>
       fetch(url)
     ))
@@ -174,43 +178,6 @@ const FetchTickets = async (user_tickets) => {
     throw err;
   }
 };
-
-function GetTickets(user_tickets)
-{
-  const activeTickets = []
-  const usedTickets = []
-  /*
-  user_tickets.forEach(ticket => {
-    url = 'https://us-central1-qrapp-fe2f3.cloudfunctions.net/encryptTicket?id=' + ticket
-    data = fetch(url,).then((response) => response.json())
-    .then((data) => {
-      if(!data.ticketExists) return;
-      const newTicket = {encryptedData: data.qrPayload.encryptedData, cost: data.ticketMeta.Cost, event: data.ticketMeta.Event, seat: data.ticketMeta.SeatNumber}
-      if(data.ticketMeta.Used) usedTickets.push(newTicket)
-      else activeTickets.push(newTicket)
-    })
-  })
-  .then((data) => {
-    console.log("E")
-  })
-  */
-
-  Promise.all(user_tickets.map(ticket => {
-    url = 'https://us-central1-qrapp-fe2f3.cloudfunctions.net/encryptTicket?id=' + ticket
-    fetch(url,).then((response) => response.json())
-    .then((data) => {
-      if(!data.ticketExists) return;
-      const newTicket = {encryptedData: data.qrPayload.encryptedData, cost: data.ticketMeta.Cost, event: data.ticketMeta.Event, seat: data.ticketMeta.SeatNumber}
-      console.log(newTicket)
-      if(data.ticketMeta.Used) usedTickets.push(newTicket)
-      else activeTickets.push(newTicket)
-    })
-  }))
-  .then(data => {
-    console.log(activeTickets)
-    console.log(usedTickets)
-  })
-}
 
 const styles = StyleSheet.create({
   container: {
